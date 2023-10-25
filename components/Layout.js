d02df1cfabb21207c586a615fc9867874dbb9f37
hasret <rabiamengi1@hotmail.com>
2023-10-25 19:28:28 -0400

    fixed logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Nav from "@/components/Nav";
 import { useState } from "react";
-import Logo from "./Logo";
+import Logo from "./logo";
 
 export default function Layout({ children }) {
   const [showNav, setShowNav] = useState(false);
